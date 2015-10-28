6313b14c3d69c289319364d1add8b3bfaff37226
Eric Bryant <ericedwardbryant@gmail.com>
2015-10-28 16:32:39 -0700

    Add src_rothfreezer and update_genes

---
+++ R/rothfreezer.R
@@ -0,0 +1,47 @@
+#-- src_rothfreezer -----------------------------------------------------------
+#' Connect to the rothfreezer database
+#' 
+#' Connect to the rothfreezer SQLite database. 
+#' 
+#' @aliases rothfreezer
+#' @importFrom RSQLite SQLite
+#' @importFrom dplyr %>% src_sqlite
+#' @importFrom easydb db_config db_build
+#' @export
+#' 
+#' @examples 
+#' # For a description of each table
+#' vignette('database-overview', package = 'rothfreezer')
+#' 
+#' # Connect to the database
+#' db <- src_rothfreezer()
+#' db
+#' 
+src_rothfreezer <- function() {
+  # Read configuration file
+  cnf <- system.file('db/_rothfreezer.yaml', package = 'rothfreezer') %>% db_config()
+  
+  # Build the database if it does not exist
+  if (!file.exists(cnf$db)) suppressWarnings(db_build(cnf))
+  
+  # Connect to the database
+  src_sqlite(cnf$db) %>% add_class('src_rothfreezer')
+}
+
+
+#-- src_rothscreen methods ----------------------------------------------------
+#' @importFrom dplyr src_desc
+#' @export
+src_desc.src_rothfreezer <- function(x) {
+  paste0(
+    'sqlite ', x$info$serverVersion, 
+    ' [rothfreezer - ', packageVersion('rothfreezer'), ']'
+  )
+}
+
+
+#-- OO Utilities --------------------------------------------------------------
+# Add an S3 subclass in pipeline
+# @param x an object
+# @param ... Character; S3 subclasses to add to object
+add_class <- function(x, ...) { class(x) <- c(..., class(x)); return(x) }
---
+++ R/update.R
@@ -0,0 +1,23 @@
+#' Update gene annotation table
+#' 
+#' Update \emph{S. cerevisiae} gene annotations from 
+#' \href{http://www.yeastgenome.org}{SGD}.
+#' 
+#' @param to Where to write the CSV
+#' 
+#' @importFrom dplyr %>% select_ 
+#' @importFrom data.table fread
+#' @export
+
+update_genes <- function(to) {
+  'http://downloads.yeastgenome.org/curation/chromosomal_feature/SGD_features.tab' %>%
+    fread(data.table = FALSE) %>%
+    select_(
+      gene_id = ~V1,
+      orf_id  = ~V4,
+      gene_name  = ~V5,
+      gene_alias = ~V6,
+      gene_description = ~V16
+    ) %>%
+    write.csv(to, row.names = FALSE)
+}
