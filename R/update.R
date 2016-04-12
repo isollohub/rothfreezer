954cfe8adeedb4584055fc304fc9b1553777c5f5
Eric Bryant <eeb2139@columbia.edu>
2016-04-12 13:56:35 -0400

    New documenation and implementation of update_genes

--- R/update.R
+++ R/update.R
@@ -5,18 +5,21 @@
 #' 
 #' @param to Where to write the CSV
 #' 
-#' @importFrom data.table fread
+#' @importFrom readr read_tsv write_csv
 #' @export
 
 update_genes <- function(to) {
   'http://downloads.yeastgenome.org/curation/chromosomal_feature/SGD_features.tab' %>%
-    fread(data.table = FALSE) %>%
-    select_(
-      gene_id = ~V1,
-      orf_id  = ~V4,
-      gene_name  = ~V5,
-      gene_alias = ~V6,
-      gene_description = ~V16
+    read_tsv(col_names = FALSE) %>%
+    col_names(
+      'gene_id', 'type', 'qualifier', 'orf_id', 'gene_name', 'gene_alias',
+      'parent_feature_name', 'secondary_sgd_id', 'chromosome', 'start', 'stop',
+      'strand', 'genetic_position', 'coordinate_version', 'sequence_version', 
+      'gene_description'
     ) %>%
-    write.csv(to, row.names = FALSE)
+    mutate_(gene_name = ~ifelse(gene_name == '', orf_id, gene_name)) %>%
+    select_(~gene_id, ~orf_id, ~gene_name, ~gene_alias, ~gene_description) %>%
+    write_csv(to)
 }
+
+col_names <- function(df, ...) { names(df) <- c(...); return(df) }
