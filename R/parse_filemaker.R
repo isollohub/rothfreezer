f8a4e27367c8e720922761314a1bd97e26840944
Eric Bryant <eeb2139@columbia.edu>
2016-04-12 12:51:13 -0400

    Document and modify parse filemaker

--- R/parse_filemaker.R
+++ R/parse_filemaker.R
@@ -1,9 +1,18 @@
-library(readr)
-library(tidyr)
-library(dplyr)
+#' @title Parse filemaker strains DB
+#'
+#' @description Parse the filemaker strains DB from an exported CSV.
+#'
+#' @param file Path to filemaker CSV.
+#' @param write Should the resulting tables be written. Defaults to \code{FALSE}.
+#'
+#'
+#' @importFrom readr read_csv write_csv cols col_character
+#' @export
 
-parse_filemaker_export <- function( file , write = F ) {
-  
+
+
+file = '/Volumes/RothsteinLab/ Eric/_Notebook/2016/04/2016-04-06-strains-db-export.csv'
+parse_filemaker_export <- function(file , write = TRUE, blame = TRUE) {
 
   strains <-
     read_csv(
@@ -15,17 +24,34 @@ parse_filemaker_export <- function( file , write = F ) {
                     'parent1', 'parent2', 'parents'),
       col_types = cols(.default = col_character()),
       na = c('', 'NA', ' ', '"', '""', '"""', '""""')
-    )
-
-  strains$entry <- 1:nrow(strains)
+    ) %>%
+    mutate(entry = 1:n())
   
+  # Enforce Eric's Dogma
   cleanish <-
     strains %>%
-    mutate_each(funs(gsub('\v.*', '', .)), set, number, extension, shelf) %>% # Remove everything after a \v (appears to behave like a newline)
-    mutate_each(funs(gsub('[^0-9]','', .)), shelf, rack, pullout, insert)  %>% # Yes, some entries do have weird characters next to the shelf/rack/pullout number
-    mutate_each(funs(substr(., 1, 1)), shelf, rack, pullout, insert, row, column) %>%      # After checking, only first number is relevant
-    mutate_each(funs(toupper(.)), row, column)  %>%
-    mutate_each(funs(suppressWarnings(ifelse(is.na(as.integer(.)), match(.,LETTERS), .))) , row, column) %>%
+    mutate(
+      row     = suppressWarnings(ifelse(is.na(as.integer(row)),    match(toupper(row), LETTERS), row)),
+      column  = suppressWarnings(ifelse(is.na(as.integer(column)), match(toupper(column),LETTERS), column)),
+      row     = suppressWarnings(as.integer(row)),
+      column  = suppressWarnings(as.integer(column)),
+      shelf   = suppressWarnings(as.integer(shelf)),
+      rack    = suppressWarnings(as.integer(rack)),
+      pullout = suppressWarnings(as.integer(pullout)),
+      insert  = suppressWarnings(as.integer(insert))
+    ) %>%
+    filter(
+      !is.na(number),
+      !is.na(shelf),
+      !is.na(rack),
+      !is.na(pullout),
+      !is.na(insert),
+      !is.na(row),
+      !is.na(column),
+      !grepl('\\s', set),
+      !grepl('\\s', extension),
+      !grepl('\\s', number)
+    ) %>%
     mutate(
       extension = ifelse(is.na(extension), '', extension),
       set = toupper(set),
@@ -34,73 +60,48 @@ parse_filemaker_export <- function( file , write = F ) {
       strain_id = gsub('-{1,}$', '', strain_id),
       location = paste0(shelf, rack, pullout, insert, row, column)
     ) %>%
-    mutate_each(funs(gsub('\t|\v', ' ', .))) %>%         # replace tabls with space
-    mutate_each(funs(gsub('^\\s{1,}|\\s{1,}$', '', .))) %>%  # Remove \v (?!) and leading/trailing spaces
-    filter(
-      !(set %in% c('', '?', NA)), 
-      !(is.na(as.integer(shelf))), 
-      !(is.na(as.integer(rack))),
-      !(is.na(as.integer(pullout))), 
-      !(is.na(as.integer(insert))), 
-      !(is.na(as.integer(row))), 
-      !(is.na(as.integer(column)))
-      )  # All valid strains must have a set value and a location number
+    mutate_each(funs(gsub('\t|\v', ' ', .)), genotype, notes) %>%         # replace tabs with space
+    mutate_each(funs(gsub('^\\s{1,}|\\s{1,}$', '', .)), genotype, notes) %>%  # Remove \v (?!) and leading/trailing spaces
+    filter(!(set %in% c('', '?', NA)))
 
+  # Construct sub-tables
   notsoclean <- 
     strains %>%
-    filter(!(entry %in% cleanish$entry))
+    filter(!(entry %in% cleanish$entry)) %>%
+    select(-entry)
   
-  duplicated <- 
+  duplicated <-
     cleanish %>%
     filter(duplicated(location) | duplicated(location, fromLast = T))
-  
-  cleanish <- 
+
+  clean <-
     cleanish %>%
-    filter(!(entry %in% duplicated$entry))
+    filter(!(entry %in% duplicated$entry)) %>%
+    select(-entry)
   
-  notsoclean <- select(notsoclean , -entry)
-  cleanish <- select(cleanish , -entry)
   duplicated <- select(duplicated , -entry)
 
-  addresses <- select(cleanish, strain_id, shelf, rack, pullout, insert, row, column)
-  cleanish <- cleanish %>%
-    select(-c(shelf,rack,pullout,insert,row,column)) %>%
+  addresses <- select(clean, strain_id, shelf, rack, pullout, insert, row, column)
+  
+  clean <- clean %>%
+    select(-(shelf:column)) %>%
     select(strain_id, everything())
+  
+  # Write to file
+  if (write) {
+    dir  <- dirname(file)
+    base <- gsub('\\.csv$|\\.CSV$', basename(file))
     
-    
+    write_csv(addresses,  paste0(dir, "/" , "addresses.csv"))
+    write_csv(notsoclean, paste0(dir, "/" , base, "-dirty.csv"))
+    write_csv(clean,      paste0(dir, "/" , base, "-cleaned.csv"))
+    write_csv(duplicated, paste0(dir, "/" , base, "-duplicates.csv"))
   
-  dir <- dirname(file)  
-    
-  write.csv(x = addresses, 
-            file = paste0(dir, "/" , "addresses.csv"), 
-            quote = F, 
-            row.names = F)
-  
-  if (write){
-  
-    write.csv(x = notsoclean, 
-              file = paste0(dir, "/" , gsub('[.].*', '', file), "-dirty.csv"), 
-              quote = F, 
-              row.names = F)
-    
-    write.csv(x = cleanish, 
-      file = paste0(dir, "/" , gsub('[.].*', '', file), "-cleaned.csv"), 
-      quote = F, 
-      row.names = F)
-    
-    write.csv(
-      x = duplicated,
-      file = paste0(dir, "/" , gsub('[.].*', '', file), "-duplicates.csv"),
-      quote = F,
-      row.names = F)
-  
-    message("4 csv files have been created")
-  } else { message("1 csv file has been created") }
-  
-  
-  
-  stock <- list(cleanish, notsoclean, duplicated)
+    message("Results written to ", dir)
+  }
+
+  stock <- list(clean = clean, dirty = notsoclean, duplicated = duplicated)
 
   return(stock)
-  
+
 }
